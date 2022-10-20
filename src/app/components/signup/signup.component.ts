d9b8c8b9cb0a4ed3b1ffe70f6feaae9e95a3f19a
Jannery Briceño <jannerymai@gmail.com>
2022-10-19 20:04:37 -0500

    Add modal of success when creating account #2

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -9,12 +9,13 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class SignupComponent implements OnInit {
 
-  constructor(private auth: AuthService, private route: Router) { }
+  constructor(private auth: AuthService, private route: Router, private render: Renderer2) { }
 
   ngOnInit(): void {
   }
 
   confirmInput: boolean = true;
+  @ViewChild('successModal') successModal!: ElementRef;
 
   registerUser(name: string, email: string, pass: string) {
     if(name === '' || email === '' || pass === ''){
@@ -23,12 +24,19 @@ export class SignupComponent implements OnInit {
       this.auth.signup(email, pass)
       .then((resp) => {
         console.log(resp)
+        this.confirmInput = true;
         this.auth.updateUser(name);
-        this.route.navigateByUrl('/login');
+        this.successModal.nativeElement.showModal();
       })
       .catch(() => {
+        this.confirmInput = false
         console.log('Be careful!!')
       })
     }
+  }
+
+  closeModal() {
+    this.successModal.nativeElement.close();
+    this.route.navigateByUrl('/login');
   }
 }
